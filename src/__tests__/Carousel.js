31397df31388221f233bc5caa60afab5b4196115
Valeriya Kozhynova <valko92@hotmail.com>
2017-08-21 12:18:24 -0400

    test fixed

--- src/__tests__/Carousel.js
+++ src/__tests__/Carousel.js
@@ -3,9 +3,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Carousel from '../components/pages/Carousel';
 import testJSON from '../components/data/building.json';
+import { MemoryRouter } from 'react-router-dom';
 
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<Carousel {...testJSON} />, div);
+  ReactDOM.render(<MemoryRouter><Carousel {...testJSON} /></MemoryRouter>, div);
 });
