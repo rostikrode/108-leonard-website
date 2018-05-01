ac04e3a5eece78ceffb2c01dac434d4486d104ab
Valeriya Kozhynova <valko92@hotmail.com>
2018-05-01 14:31:24 -0400

    not sure how to fix travis snapshot fail.

--- src/__tests__/Contact.js
+++ src/__tests__/Contact.js
@@ -6,6 +6,10 @@ import renderer from 'react-test-renderer';
 import Contact from '../components/pages/Contact';
 import { MemoryRouter } from 'react-router-dom';
 
+window.gtag = window.gtag || function() {
+  return true;
+};
+
 it('renders without crashing', () => {
   const tree = renderer.create(
     <MemoryRouter><Contact /></MemoryRouter>
