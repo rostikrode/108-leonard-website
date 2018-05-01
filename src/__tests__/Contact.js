37417a40a0c7c9086c952e907ae5483474283621
Valeriya Kozhynova <valko92@hotmail.com>
2018-05-01 14:16:04 -0400

    tests pass locally, but not on travis

--- src/__tests__/Contact.js
+++ src/__tests__/Contact.js
@@ -6,10 +6,6 @@ import renderer from 'react-test-renderer';
 import Contact from '../components/pages/Contact';
 import { MemoryRouter } from 'react-router-dom';
 
-window.gtag = window.gtag || function() {
-  return true;
-};
-
 it('renders without crashing', () => {
   const tree = renderer.create(
     <MemoryRouter><Contact /></MemoryRouter>
