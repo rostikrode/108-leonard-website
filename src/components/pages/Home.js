9f1bcf56bcd613e9e2692a7d2e5b0264e1c50e9e
Valeriya Kozhynova <valko92@hotmail.com>
2018-08-21 14:37:24 -0400

    no more using cookies.

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import {NavLink} from 'react-router-dom';
 import Img from 'react-image';
-import Cookies from 'js-cookie';
+// import Cookies from 'js-cookie';
 import 'velocity-animate/velocity.ui';
 import Loader from '../partials/Loader';
 import '../../styles/Home.css';
