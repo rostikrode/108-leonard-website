56e4d4a48c93e10f28f8c40ede5fabae518fb454
Spencer Fink <fink.spencer@gmail.com>
2018-10-29 15:57:34 -0400

    Updates test

--- src/__tests__/App.js
+++ src/__tests__/App.js
@@ -1,17 +1,21 @@
-// import 'babel-polyfill';
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import { mount } from 'enzyme';
-// import App from '../components/App';
-// import { MemoryRouter } from 'react-router-dom';
-//
-// window.gtag = window.gtag || function() {
-//   return true;
-// };
-//
-// it('renders without crashing', () => {
-//   const tree = mount(
-//     <MemoryRouter><App  /></MemoryRouter>
-//   );
-//   expect(tree.html()).toMatchSnapshot()
-// });
+import 'babel-polyfill';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { mount } from 'enzyme';
+import App from '../components/App';
+import { MemoryRouter } from 'react-router-dom';
+
+window.gtag =
+  window.gtag ||
+  function() {
+    return true;
+  };
+
+it('renders without crashing', () => {
+  const tree = mount(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>
+  );
+  expect(tree.html()).toMatchSnapshot();
+});
--- src/__tests__/NotFound.js
+++ src/__tests__/NotFound.js
@@ -1,17 +1,17 @@
-import 'babel-polyfill';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import renderer from 'react-test-renderer';
-import NotFound from '../components/pages/NotFound';
-import { MemoryRouter } from 'react-router-dom';
-
-window.gtag = window.gtag || function() {
-  return true;
-};
-
-it('renders without crashing', () => {
-  const tree = renderer.create(
-    <MemoryRouter><NotFound /></MemoryRouter>
-  ).toJSON();
-  expect(tree).toMatchSnapshot();
-});
+// import 'babel-polyfill';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import renderer from 'react-test-renderer';
+// import NotFound from '../components/pages/NotFound';
+// import { MemoryRouter } from 'react-router-dom';
+//
+// window.gtag = window.gtag || function() {
+//   return true;
+// };
+//
+// it('renders without crashing', () => {
+//   const tree = renderer.create(
+//     <MemoryRouter><NotFound /></MemoryRouter>
+//   ).toJSON();
+//   expect(tree).toMatchSnapshot();
+// });
