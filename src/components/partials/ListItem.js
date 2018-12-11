e09a1054ac4271a459acf0ba4e5610a1fa940970
Spencer Fink <fink.spencer@gmail.com>
2018-12-11 14:13:08 -0500

    updates fetch for dev

--- src/components/partials/ListItem.js
+++ src/components/partials/ListItem.js
@@ -63,8 +63,9 @@ export default class ListItem extends Component {
 
     // check if floorplan exists
     let exists = true;
+    // CHANGE URL FOR MASTER "108leonard-full.dev.dbxd.com -> 108leonard.com"
     fetch(
-      `https://s3.amazonaws.com/108leonard.dev.dbxd.com/images/5_availability/pdfs/residence_${
+      `https://s3.amazonaws.com/108leonard-full.dev.dbxd.com/images/5_availability/pdfs/residence_${
         this.props.residencePDFFileName
       }.pdf`
     )
