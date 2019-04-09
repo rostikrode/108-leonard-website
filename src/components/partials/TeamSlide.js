661ebef137d5f02da3f1593110dfb8a52f118c39
Spencer Fink <fink.spencer@gmail.com>
2019-04-09 12:58:20 -0400

    updates to team slide and team copy

--- src/components/partials/TeamSlide.js
+++ src/components/partials/TeamSlide.js
@@ -2,53 +2,71 @@ import React from 'react';
 import Img from 'react-image';
 import Loader from './Loader';
 
-const TextSlide = (props) => {
+const TextSlide = props => {
   return (
     <div className={`inner ${props.slide.title2 ? 'reverse' : ''}`}>
-    {props.slide.images ? 
-      <div className="image-group">
-    
+      {props.slide.images ? (
+        <div className="image-group">
           {props.slide.images.map((image, key) => {
             return (
-              <div key={key} className={`image-item ${image.half ? 'half' : 'full'}`}>
+              <div
+                key={key}
+                className={`image-item ${image.half ? 'half' : 'full'}`}
+              >
                 <div
                   className="image"
                   title={image.caption}
                   style={{
                     backgroundImage: `url(${image.url})`
-                  }} 
+                  }}
                 />
-                <Img className="image-tag" src={image.url} loader={<Loader />} alt={image.caption} onLoad={()=> {props.onIntroImgLoad();}} />
+                <Img
+                  className="image-tag"
+                  src={image.url}
+                  loader={<Loader />}
+                  alt={image.caption}
+                  onLoad={() => {
+                    props.onIntroImgLoad();
+                  }}
+                />
                 <p className="caption serif-bold">{image.caption}</p>
               </div>
-              
             );
           })}
-          </div>
-        : 
-          ''
-        }
+        </div>
+      ) : (
+        ''
+      )}
 
       <div className="caption-wrapper">
         <div className="wrapper">
           <h1 className="title sans-bold">{props.slide.title}</h1>
-          <h6 className="subtitle serif" dangerouslySetInnerHTML={{__html: props.slide.subtitle}}></h6>
-          <p className="serif">{props.slide.para}</p>
+          <h6
+            className="subtitle serif"
+            dangerouslySetInnerHTML={{ __html: props.slide.subtitle }}
+          />
+          <p
+            className="serif"
+            dangerouslySetInnerHTML={{ __html: props.slide.para }}
+          />
         </div>
       </div>
-      {props.slide.title2 ?
+      {props.slide.title2 ? (
         <div className="caption-wrapper second-team">
           <div className="wrapper">
             <h1 className="title sans-bold">{props.slide.title2}</h1>
             <h6 className="subtitle serif">{props.slide.subtitle2}</h6>
-            <p className="serif" dangerouslySetInnerHTML={{__html: props.slide.para2}} />
+            <p
+              className="serif"
+              dangerouslySetInnerHTML={{ __html: props.slide.para2 }}
+            />
           </div>
         </div>
-        :
+      ) : (
         ''
-      }
+      )}
     </div>
   );
-}
+};
 
 export default TextSlide;
