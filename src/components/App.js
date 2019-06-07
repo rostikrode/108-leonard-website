9063a72a5e8171134d060f2dccd0b1eb61e2bfd7
Joel Hoelting <joelhoelting@protonmail.com>
2019-06-07 12:11:03 -0400

    Add skip links

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { Switch, Route, Redirect } from "react-router-dom";
 import Cookies from "js-cookie";
 import NotFound from "./pages/NotFound";
+import SkipLinks from './partials/SkipLinks';
 import Header from "./partials/Header";
 // import Animation from './partials/Animation';
 import "../styles/App.css";
@@ -285,6 +286,7 @@ export default class App extends Component {
     });
     return (
       <div className="App">
+        <SkipLinks />
         <h1 className="visuallyhidden">108 Leonard</h1>
         <Header
           pages={PAGES}
--- src/components/pages/Accessibility.js
+++ src/components/pages/Accessibility.js
@@ -44,12 +44,14 @@ export default class Accessibility extends Component {
   render() {
     return (
       <div className="accessibility-wrapper">
-        {this.props.body.map((section, idx) => (
-          <div key={`accessibility-section-${idx}`} className="content sans">
-            <h3 style={{ fontWeight: "bold" }}>{section.title}</h3>
-            <p>{section.content}</p>
+          <div className="content">
+            <h3 style={{ fontWeight: "bold", textTransform: 'uppercase' }}>Website Accessibility Statement</h3>
+            <p>108 Leonard is committed to providing a website that is accessible to the widest possible audience in accordance with ADA standards and WCAG guidelines. We are actively working to increase accessibility and usability of our website to everyone.  108 Leonard is committed to providing a positive experience to all of our customers and employees, and we strive to promote accessibility and inclusion. Whether you are using assistive technologies like a screen reader, a magnifier, voice recognition software, or captions for videos, our goal is to make your visit to 108 Leonard’s website a successful and enjoyable experience.</p>
           </div>
-        ))}
+          <div className="content">
+            <h3 style={{ fontWeight: "bold", textTransform: 'uppercase' }}>Accessibility Assistance</h3>
+            <p>If you are using a screen reader or other auxiliary aid or are having difficulty using or accessing any element of this website, please feel free to call us at <a href="tel:2127751080">(212) 775-1080</a> or email us at <a href="mailto:info@108leonard.com">info@108leonard.com</a>. We will work with you to provide the information or service you seek through a communication method that is accessible for you consistent with applicable law (for example, through telephone support).</p>
+          </div>
       </div>
     );
   }
