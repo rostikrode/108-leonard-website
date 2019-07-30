2966e2f636bdf29072065586e2f32f4dd848f7e2
Spencer Fink <fink.spencer@gmail.com>
2019-07-30 10:36:07 -0400

    Fixes PressArticle page

--- src/components/pages/PressArticle.js
+++ src/components/pages/PressArticle.js
@@ -1,109 +1,130 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import close_thin_blue from '../../assets/close_thin_blue.svg';
 
 export default class PressArticle extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      article: this.slugify(this.props.match ? this.props.match.params.article: ''),
-      publication: this.slugify(this.props.match ? this.props.match.params.publication: ''),
+      article: this.slugify(
+        this.props.match ? this.props.match.params.article : ''
+      ),
+      publication: this.slugify(
+        this.props.match ? this.props.match.params.publication : ''
+      ),
       entry: {},
       overflow: '',
       title: this.props.metaTitle,
       desc: this.props.metaDescription
-    }
+    };
   }
   componentWillMount() {
     if (window.location.origin === 'https://108leonard.com') {
       window.gtag('config', 'UA-113369414-1', {
-        'page_title': this.state.title,
-        'page_location': window.location.href,
-        'page_path': window.location.pathname
+        page_title: this.state.title,
+        page_location: window.location.href,
+        page_path: window.location.pathname
       });
     }
-  }
-  componentDidMount() {
-    /** meta data for page */
-    document.title = this.state.title;
-    if(document.getElementsByTagName('meta').description) {
-      document.getElementsByTagName('meta').description.content = this.state.desc;
-      document.querySelector("meta[property='og:description']").content = this.state.desc;
-      document.querySelector("meta[property='og:title']").content = this.state.title;
-    }
-    if (document.querySelector("link[rel='canonical']")) {
-      document.querySelector("link[rel='canonical']").href = window.location.href
-      document.querySelector("meta[property='og:url']").content = window.location.href
-    }
-    var viewport = document.querySelector("meta[name=viewport]");
-    if(viewport) {
-      viewport.setAttribute('content', 'width=device-width, initial-scale=1, user-scalable=1');
-    }
-
-    // getting the correct article by matching publisher and article title
-    this.props.articles.forEach((value, index) => {
-      var artslug = this.slugify(this.slugify(value.title));
-      var pubslug = this.slugify(this.slugify(value.publisher));
-      if ((artslug === this.state.article) && (pubslug === this.state.publication)) {
-        this.setState({
-          entry: value,
-          title: value.metatitle,
-          desc: value.metadesc
-        });
-      }
-    });
   }
 
   componentDidUpdate() {
     if (window.location.origin === 'https://108leonard.com') {
       window.gtag('config', 'UA-113369414-1', {
-        'page_title': this.state.title,
-        'page_location': window.location.href,
-        'page_path': window.location.pathname
+        page_title: this.state.title,
+        page_location: window.location.href,
+        page_path: window.location.pathname
       });
-    }
-    
-    document.title = this.state.title;
-    if(document.getElementsByTagName('meta').description) {
-      document.getElementsByTagName('meta').description.content = this.state.desc;
-      document.querySelector("meta[property='og:description']").content = this.state.desc;
-      document.querySelector("meta[property='og:title']").content = this.state.title;
-    }
-    if (document.querySelector("link[rel='canonical']")) {
-      document.querySelector("link[rel='canonical']").href = window.location.href
-      document.querySelector("meta[property='og:url']").content = window.location.href
     }
   }
 
   slugify(text) {
-    return text.toString().toLowerCase()
-      .replace(/\s+/g, '-')           // Replace spaces with -
-      .replace(/[^\w-]+/g, '')       // Remove all non-word chars
-      .replace(/--+/g, '-')         // Replace multiple - with single -
-      .replace(/^-+/, '')             // Trim - from start of text
-      .replace(/-+$/, '');            // Trim - from end of text
+    return text
+      .toString()
+      .toLowerCase()
+      .replace(/\s+/g, '-') // Replace spaces with -
+      .replace(/[^\w-]+/g, '') // Remove all non-word chars
+      .replace(/--+/g, '-') // Replace multiple - with single -
+      .replace(/^-+/, '') // Trim - from start of text
+      .replace(/-+$/, ''); // Trim - from end of text
   }
 
   closeBtnClick() {
-    console.log('beh');
     this.props.history.push('/press/');
   }
 
   render() {
-    return (
-      <div className='press-article' ref={e => {this.article = e;}}>
-        <button className="close-btn" onClick={this.closeBtnClick.bind(this)}><img src={close_thin_blue} alt="close btn" className="close-btn-img" width="25" height="25" /></button>
+    let article;
+    if (this.props.pressArticles) {
+      article = this.props.pressArticles.find(article => {
+        var artslug = this.slugify(this.slugify(article.title));
+        var pubslug = this.slugify(this.slugify(article.publisher));
+        if (
+          artslug === this.state.article &&
+          pubslug === this.state.publication
+        ) {
+          return article;
+        }
+      });
+    }
 
-        <div className="press-item not-active">
-          <h2 className="publication sans-light-bold upper">{this.state.entry.publisher}</h2>
-          <h5 className="title sans">{this.state.entry.title}</h5>
-          <h5 className="date sans">{this.state.entry.date}</h5>
+    if (article) {
+      document.title = article.title;
+      if (document.getElementsByTagName('meta').description) {
+        document.getElementsByTagName('meta').description.content =
+          article.subtitle;
+        document.querySelector("meta[property='og:description']").content =
+          article.summary;
+        document.querySelector("meta[property='og:title']").content =
+          article.title;
+      }
+      if (document.querySelector("link[rel='canonical']")) {
+        document.querySelector("link[rel='canonical']").href =
+          window.location.href;
+        document.querySelector("meta[property='og:url']").content =
+          window.location.href;
+      }
+
+      let { publisher, title, date, summary, subtitle, pdf } = article;
+      date = date.replace(/\//g, '.');
+
+      return (
+        <div
+          className="press-article"
+          ref={e => {
+            this.article = e;
+          }}
+        >
+          <button className="close-btn" onClick={this.closeBtnClick.bind(this)}>
+            <img
+              src={close_thin_blue}
+              alt="close btn"
+              className="close-btn-img"
+              width="25"
+              height="25"
+            />
+          </button>
+
+          <div className="press-item not-active">
+            <h2 className="publication sans-light-bold upper">{publisher}</h2>
+            <h5 className="title sans">{title}</h5>
+            <h5 className="date sans">{date}</h5>
+          </div>
+          <div
+            className="summary-wrapper"
+            ref={e => {
+              this.summary = e;
+            }}
+          >
+            <h1 className="subtitle sans-bold">{subtitle}</h1>
+            <p className="summary serif">{summary}</p>
+            <a className="url serif-bold upper" href={pdf} target="_blank">
+              DOWNLOAD FULL ARTICLE
+            </a>
+          </div>
         </div>
-        <div className="summary-wrapper" ref={e => {this.summary = e;}}>
-          <h1 className="subtitle sans-bold">{this.state.entry.subtitle}</h1>
-          <p className="summary serif">{this.state.entry.summary}</p>
-          <a className="url serif-bold upper" href={this.state.entry.link} target="_blank">DOWNLOAD FULL ARTICLE</a>
-        </div>
-      </div>
-    );
+      );
+    } else {
+      return <div className="press-article" />;
+    }
   }
 }
